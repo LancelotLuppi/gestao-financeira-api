ff9a38ab5f2debc5c9f20cae139b6c1bca2626cb
Gabriel Luppi <luppi.gabriel08@gmail.com>
2022-09-05 16:14:10 -0300

    Adicionando estrutura BalanceEntity

---
+++ src/main/java/com/luppi/gestaofinanceira/entity/EarningEntity.java
@@ -0,0 +1,35 @@
+package com.luppi.gestaofinanceira.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "earning")
+public class EarningEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_EARNING")
+    @SequenceGenerator(name = "SEQ_EARNING", sequenceName = "seq_id_earning", allocationSize = 1)
+    @Column(name = "id_earning")
+    private Integer idEarning;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "value")
+    private BigDecimal value;
+
+    @Column(name = "income_date")
+    private Integer incomeDate;
+
+    @Column(name = "id_balance")
+    private BalanceEntity balance;
+}
