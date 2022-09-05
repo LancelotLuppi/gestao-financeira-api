b5412f16be1940f7f00f7ee01576efb163b417bf
Gabriel Luppi <luppi.gabriel08@gmail.com>
2022-09-05 16:03:46 -0300

    Adicionando estrutura da UserEntity

---
+++ src/main/java/com/luppi/gestaofinanceira/entity/UserEntity.java
@@ -0,0 +1,34 @@
+package com.luppi.gestaofinanceira.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_USER")
+    @SequenceGenerator(name = "SEQ_USER", sequenceName = "seq_id_user", allocationSize = 1)
+    @Column(name = "id_user", insertable = false, updatable = false)
+    private Integer idUser;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "id_balance")
+    private Integer idBalance;
+}
