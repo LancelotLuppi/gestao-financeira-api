d2f65b9efa48cec60516224a30cfd8af2e3b1d61
Gabriel Luppi <luppi.gabriel08@gmail.com>
2022-09-05 18:06:56 -0300

    Adicionando exceptions e authentication service

---
+++ src/main/java/com/luppi/gestaofinanceira/exception/BusinessException.java
@@ -0,0 +1,7 @@
+package com.luppi.gestaofinanceira.exception;
+
+public class BusinessException extends Exception {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/luppi/gestaofinanceira/security/AuthenticationService.java
+++ src/main/java/com/luppi/gestaofinanceira/security/AuthenticationService.java
@@ -1,19 +1,18 @@
 package com.luppi.gestaofinanceira.security;
 
+import com.luppi.gestaofinanceira.exception.EntityNotFoundException;
 import com.luppi.gestaofinanceira.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-import org.webjars.NotFoundException;
-
-import java.nio.file.attribute.UserPrincipalNotFoundException;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
     private final UserRepository userRepository;
 
-    public UserDetails loadUserByUsername(String username) throws UserPrincipalNotFoundException {
-        return userRepository.findByUsername(username).get();
+    public UserDetails loadUserByUsername(String username) throws EntityNotFoundException {
+        return userRepository.findByUsername(username).
+                orElseThrow(() -> new EntityNotFoundException("Usuario nao encontrado"));
     }
 }
